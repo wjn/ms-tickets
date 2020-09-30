cc84b9de0a225cb08f679e9e7bfde8f6911c1b50
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-09-30 07:29:29 -0500

    set up TDD

---
+++ src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('should have a route handler listening to /api/tickets for POST requests', async () => {});
+it('should only be accessed if user is authenticated', async () => {});
+it('should return an error if invalid title is provided', async () => {});
+it('should return an error if invalid price is provided', async () => {});
+it('should create a ticket with valid inputs', async () => {});
