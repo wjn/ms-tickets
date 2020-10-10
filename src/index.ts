58cddfa4ca4f01608326fd1fdd6c8f08374207e3
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-09 20:16:24 -0500

    added logIt entries

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,57 @@
 import mongoose from 'mongoose';
 import { app } from './app';
-import { NotFoundError } from '@nielsendigital/ms-common';
+import { NotFoundError, logIt, LogType } from '@nielsendigital/ms-common';
+import { natsWrapper } from './nats-wrapper';
 
 const startApp = async () => {
+  // verify env vars are present
+
   if (!process.env.JWT_KEY) {
     throw new NotFoundError('JWT_KEY k8s secret must be defined.');
   }
   if (!process.env.MONGO_URI) {
     throw new NotFoundError('MONGO_URI k8s env var must be defined.');
   }
+
+  logIt.out(LogType.INFO, 'All required ENV Vars verified as defined');
+
+  // connect to NATS
+
+  try {
+    await natsWrapper.connect(
+      'ticketing',
+      'asdfasf222',
+      'http://nats-srv:4222'
+    );
+    // process graceful exit
+    natsWrapper.client.on('close', () => {
+      logIt.out(LogType.STOPPED, 'NATS Connection Closed');
+      process.exit();
+    });
+    // gracefully exit
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    logIt.out(LogType.ERROR, 'NATS failed to load.');
+    logIt.out(LogType.ERROR, err);
+  }
+
+  // connect to MongoDB
+
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected to mongoDB');
+    logIt.out(LogType.SUCCESS, 'Connected to mongoDB');
   } catch (err) {
-    console.log(err);
+    logIt.out(LogType.ERROR, err);
   }
+
+  // Listen for traffic
   app.listen(3000, () => {
-    console.log('>>>>> Ticket service listening on port 3000.');
+    logIt.out(LogType.INFO, '>>>>> Ticket service listening on port 3000.');
   });
 };
 
--- src/routes/show.ts
+++ src/routes/show.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
-import { NotFoundError } from '@nielsendigital/ms-common';
+import { logIt, LogType, NotFoundError } from '@nielsendigital/ms-common';
 
 const router = express.Router();
 
 router.get('/api/tickets/:id', async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
   if (!ticket) {
+    logIt.out(LogType.ERROR, 'Ticket not found');
     throw new NotFoundError('Ticket not found');
   }
   res.status(200).send(ticket);
