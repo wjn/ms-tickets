493b6d94ad5292d558bc012608f95fb1f250c4ed
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-10 10:23:01 -0500

    updated ms-common and added k8s env vars for NATS params

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { NotFoundError, logIt, LogType } from '@nielsendigital/ms-common';
 import { natsWrapper } from './nats-wrapper';
 
 const startApp = async () => {
+  console.log('tickets service starting');
+  logIt.out(LogType.STARTED, 'tickets service started');
   // verify env vars are present
 
   if (!process.env.JWT_KEY) {
@@ -13,21 +15,38 @@ const startApp = async () => {
     throw new NotFoundError('MONGO_URI k8s env var must be defined.');
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new NotFoundError('NATS_CLUSTER_ID k8s env var must be defined.');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new NotFoundError('NATS_CLIENT_ID k8s env var must be defined.');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new NotFoundError('NATS_URL k8s env var must be defined.');
+  }
+
   logIt.out(LogType.INFO, 'All required ENV Vars verified as defined');
 
   // connect to NATS
 
   try {
+    logIt.out(LogType.INFO, 'Attempting connection to NATS');
+
     await natsWrapper.connect(
-      'ticketing',
-      'asdfasf222',
-      'http://nats-srv:4222'
+      // see infra/k8s/tickets-depl.yaml for values
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     );
+
     // process graceful exit
     natsWrapper.client.on('close', () => {
       logIt.out(LogType.STOPPED, 'NATS Connection Closed');
       process.exit();
     });
+
     // gracefully exit
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
@@ -39,6 +58,8 @@ const startApp = async () => {
   // connect to MongoDB
 
   try {
+    logIt.out(LogType.INFO, 'Attempting connection to MongoDB');
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
